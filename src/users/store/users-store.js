49f1aba03da97dba33aad4d1dc5891c9e1ba59a1
Carlos R. Primorac <carlosprimorac@gmail.com>
2024-02-08 19:51:59 -0300

    commit inicial

---
+++ src/users/store/users-store.js
@@ -0,0 +1,33 @@
+import { loadUsersByPage } from "../use-cases/load-users-by-page";
+
+
+const state = {
+    currentPage: 0,
+    users: [],
+}
+
+const loadNextPage = async() => {
+    await loadUsersByPage( state.currentPage + 1);
+}
+
+const loadPreviousPage = async() => {
+    throw new Error('no implementado');
+}
+
+const onUserChanged = () => {
+    throw new Error('no implementado');
+}
+
+const reloadPage = async() => {
+    throw new Error('no implementado');
+}
+
+ export default {
+    loadNextPage,
+    loadPreviousPage,
+    onUserChanged,
+    reloadPage,
+
+    getUser: () => [...state.users],
+    getCurrentPage: () => state.currentPage
+ }
---
+++ src/users/users-app.js
@@ -0,0 +1,12 @@
+import usersStore from "./store/users-store";
+
+/**
+ * 
+ * @param {HTMLElement} element 
+ */
+export const UsersApp = async( element ) => {
+
+    element.innerHTML = 'Loading...';
+    await usersStore.loadNextPage();
+
+}
